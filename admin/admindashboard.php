cffead0e7406288aa77475acccba20c81f952925
PreityE <Preityantoinette0521@gmail.com>
2023-10-31 15:43:42 +0800

    okay na

--- admin/admindashboard.php
+++ admin/admindashboard.php
@@ -41,21 +41,7 @@
         <div class="sidebar-title">
 
           <div class="sidebar-brand">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="currentColor"
-                class="bi bi-clipboard-check"
-                viewBox="0 0 16 16">
-              <path
-                fill-rule="evenodd"
-                d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-              <path
-                d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-              <path
-                d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-              </svg>  Admin
+            <span class="material-icons-outlined">inventory</span> Admin
           </div>
 
           <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
@@ -64,11 +50,10 @@
         <ul class="sidebar-list">
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
+
+            
             <a href="#">
-=======
-            <a href="#" target="_blank">
-<<<<<<< HEAD
+
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
@@ -81,31 +66,15 @@
                     d="M6 1v3H1V1h5zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12v3h-5v-3h5zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8v7H1V8h5zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6v7h-5V1h5zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z"
                     stroke-width="3"/>
                 </svg>  Dashboard
-=======
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
-            <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="25"
-                        height="20"
-                        fill="currentColor"
-                        class="bi bi-arrow-left"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M17.431,2.156h-3.715c-0.228,0-0.413,0.186-0.413,0.413v6.973h-2.89V6.687c0-0.229-0.186-0.413-0.413-0.413H6.285c-0.228,0-0.413,0.184-0.413,0.413v6.388H2.569c-0.227,0-0.413,0.187-0.413,0.413v3.942c0,0.228,0.186,0.413,0.413,0.413h14.862c0.228,0,0.413-0.186,0.413-0.413V2.569C17.844,2.342,17.658,2.156,17.431,2.156 M5.872,17.019h-2.89v-3.117h2.89V17.019zM9.587,17.019h-2.89V7.1h2.89V17.019z M13.303,17.019h-2.89v-6.651h2.89V17.019z M17.019,17.019h-2.891V2.982h2.891V17.019z"
-                        />
-                    </svg>
-                    Dashboard
->>>>>>> refs/remotes/origin/main
+
+            
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
+
             <a href="calendar.php">
-=======
-<<<<<<< HEAD
+
             <a href="#" target="_blank">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -119,20 +88,14 @@
                 <path
                     d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" stroke-width="3"/>
                 </svg>  Calendar
-=======
-            <a href="Calendar.php" target="_blank">
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
-              <span class="material-icons-outlined">inventory_2</span> Calendar
->>>>>>> refs/remotes/origin/main
-            </a>
+
+            
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
-            <a href="Scheduledtask.php">
-              <span class="material-icons-outlined">fact_check</span> Scheduled Tasks
-=======
-            <a href="#" target="_blank">
+
+
+            <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="23"
@@ -144,16 +107,13 @@
                     d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                 <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                 </svg>  Scheduled Tasks
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
-            <a href="services.php">
-              <span class="material-icons-outlined">add_shopping_cart</span> Services
-=======
-            <a href="#" target="_blank">
+
+            <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="23"
@@ -166,16 +126,13 @@
                 <path
                     d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                 </svg>  Services
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
-            <a href="userlist.php">
-              <span class="material-icons-outlined">shopping_cart</span> User lists
-=======
-            <a href="#" target="_blank">
+
+            <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="22"
@@ -186,16 +143,13 @@
                 <path
                     d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
                 </svg>  User lists
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
-            <a href="accounts.php">
-              <span class="material-icons-outlined">poll</span> Accounts
-=======
-            <a href="#" target="_blank">
+
+            <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -206,16 +160,13 @@
                 <path
                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
                 </svg>  Accounts
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
-            <a href="settings.php">
-              <span class="material-icons-outlined">settings</span> Settings
-=======
-            <a href="#" target="_blank">
+
+            <a href="#">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="23"
@@ -228,16 +179,13 @@
                 <path
                     d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
                 </svg>  Settings
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
           <li class="sidebar-list-item">
-<<<<<<< HEAD
+
             <a href="logout.php">
-              <span class="material-icons-outlined">settings</span> Log out
-=======
-            <a href="logout.php" target="_blank">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -252,7 +200,7 @@
                     fill-rule="evenodd"
                     d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                 </svg>  Log out
->>>>>>> 5e4c6d3f116be6544b856f86619a01f17e800ad4
+
             </a>
           </li>
 
