187f28947f3e902cf3b67f9191626be6400594fc
PreityE <Preityantoinette0521@gmail.com>
2023-11-12 15:19:15 +0800

    fixed website

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="assets/global/css/design.css">
     <link rel="stylesheet" href="assets/css/style3.css">
     <link rel="stylesheet" href="assets/css/lightbox.css">
+    <link rel="stylesheet" href="assets/css/carousel.css">
 </head>
 
 <body>
@@ -43,6 +44,20 @@
         <h3>Lagring studio</h3>
         <span>Photography</span>
         <a href="Login.php" class="btn">Schedule now</a>
+
+        <div class="carousel-container">
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" id= "prevBtn"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" id= "nextBtn"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            <div class="carousel-slide">
+                <img src="assets/images/carousel5.jpg" id="lastClone" alt="">
+                <img src="assets/images/carousel1.jpg" alt="">
+                <img src="assets/images/carousel2.jpg" alt="">
+                <img src="assets/images/carousel3.jpg" alt="">
+                <img src="assets/images/carousel4.jpg" alt="">
+                <img src="assets/images/carousel5.jpg" alt="">
+                <img src="assets/images/carousel1.jpg" id="firstClone" alt="">
+            </div>
+        </div>
     </div>
 </section>
 
@@ -110,6 +125,7 @@
     </div>
 </div>
     <script src="assets/js/lightbox-plus-jquery.js"></script>
+    <script src="assets/js/carousel.js"></script>
 </div>
 
 <!-- Gallery section ends -->
