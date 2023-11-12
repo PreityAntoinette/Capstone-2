bb855d967cab2d8c86ef81bab2e52a636e3340fb
PreityE <Preityantoinette0521@gmail.com>
2023-11-12 21:56:25 +0800

    fixed carousel

--- carousel.php
+++
@@ -1,9 +0,0 @@
-<div class="carousel-container">
-    <div class="carousel-slide">
-        <img src="../assets/images/carousel1.jpg" alt="">
-        <img src="../assets/images/carousel2.jpg" alt="">
-        <img src="../assets/images/carousel3.jpg" alt="">
-        <img src="../assets/images/carousel4.jpg" alt="">
-        <img src="../assets/images/carousel5.jpg" alt="">
-    </div>
-</div>
