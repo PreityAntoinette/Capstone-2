ff690378b25ec81c167c66c799b3af9ca99169ca
peytpeyt <maquermefaith@gmail.com>
2023-11-13 16:08:00 +0800

    naglagay ako ng table sa serivices, connected narin siya sa database

--- admin/services.php
+++ admin/services.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet" />
         <!-- Custom CSS -->
         <link rel="stylesheet" href="../assets/global/css/design.css" />
+        <link rel="stylesheet" href="../assets/global/css/table.css" />
         <link rel="stylesheet" href="../assets/admin/css/dashboardcontainer.css" />
     </head>
     <body>
@@ -24,9 +25,122 @@
             <div class="main-title">
                 <p class="font-weight-bold">SERVICES</p>
             </div>
+
+        <!-- options to set the row count -->
+                    <div class= "showAndSearch  text-secondary align-items-center mb-1">
+                        
+                        <div class= "align-items-center">
+                            <label for="searchInput" class="pr-1" >Search:</label>
+                            <div style="position: relative;">
+                                <input type="text" id="searchInput" >
+                                <a id="clearButton" class=""> &times;</a>
+                            </div>
+                        </div>
+        <!-- Table for Services Offer -->
+                        <div id="table1" class="table-group-container active">
+                            <table id="table1" class="table table-striped table-bordered w-100">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Services</th>
+                                        <th>Description</th>
+                                        <th>Price</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                    $i = 1;
+                                    $sql = mysqli_query($connection, "SELECT * FROM services") or die(mysqli_error($connection));
+                                    if (mysqli_num_rows($sql) > 0) {
+                                    while ($row = mysqli_fetch_array($sql)) {
+                                        $id = $row['service_id'];
+                                        $sername = $row['service_name'];
+                                        $serdesc = $row['service_description'];
+                                        $serprice = $row['service_price'];
+                                        $registeredDate = date("M j, Y", strtotime($row['registration_date']));
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $i++ ?></td>
+                                        <td><?php echo strtoupper($sername);?></td>
+                                        <td><?php echo strtoupper($serprice);?></td>
+                                        <td><?php echo $serdesc; ?></td>
+                                        <td class="justify-space-evenly">
+                                            <!-- view button -->
+                                            <a
+                                                href="#"
+                                                title="View"
+                                                class="modal-trigger justify-content-center"
+                                                data-modal-id="<?php echo 'Services'.$id; ?>">
+                                                <button class="p-1 m-0 btn btn-warning">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="25"
+                                                        height="25"
+                                                        fill="currentColor"
+                                                        class="bi bi-eye-fill"
+                                                        viewBox="0 0 16 16">
+                                                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                                        <path
+                                                            d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                                    </svg>
+                                                </button>
+                                            </a>
+                                            
+                                            <a
+                                                href="#"
+                                                rel="tooltip"
+                                                title="archive"
+                                                class="modal-trigger"
+                                                data-modal-id="<?php echo 'archive_announcements'; ?>">
+                                                    <button class="p-1 m-1 btn btn-secondary">
+                                                        <svg
+                                                            width="25px"
+                                                            height="25px"
+                                                            viewBox="0 0 24 24"
+                                                            fill="none" >
+                                                            <path
+                                                                opacity="0.4"
+                                                                d="M14.5 10.6499H9.5"
+                                                                stroke="#e8e8e8"
+                                                                stroke-width="1.5"
+                                                                stroke-miterlimit="10"
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round">
+                                                            </path>
+                                                            <path
+                                                                d="M16.8203 2H7.18031C5.05031 2 3.32031 3.74 3.32031 5.86V19.95C3.32031 21.75 4.61031 22.51 6.19031 21.64L11.0703 18.93C11.5903 18.64 12.4303 18.64 12.9403 18.93L17.8203 21.64C19.4003 22.52 20.6903 21.76 20.6903 19.95V5.86C20.6803 3.74 18.9503 2 16.8203 2Z"
+                                                                stroke="#e8e8e8"
+                                                                stroke-width="1.8"
+                                                                stroke-linecap="round"
+                                                                stroke-linejoin="round">
+                                                            </path>
+                                                        </svg>
+                                                    </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                        // include('services_modals');
+                                    }}
+                                    else {echo '<tr><td colspan="5" style="text-align: center;">No records found.</td></tr>';}
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <!-- table pagination -->
+                        <div class="pagination-container">
+                            <div id="pagination-label"></div>
+                            <div id="pagination" class="pagination"></div>
+                        </div>
+                    </div>
+                </div>
+
         </main>
             <!-- End Main -->
         <!-- Custom JS -->
         <script src="../assets/admin/js/sidebar_toggle.js"></script>
+        <script src="../assets/global/js/table.js"></script>
     </body>
 </html>
