899d6eb7ee707dd97c7246dc4fcc612deadfb76a
kentysss <tapawan555@gmail.com>
2023-11-23 21:11:13 +0800

    View Info Modal And Delete Button Modal

--- admin/scheduled_task.php
+++ admin/scheduled_task.php
@@ -12,8 +12,54 @@
         <link rel="stylesheet" href="../assets/global/css/design.css" />
         <link rel="stylesheet" href="../assets/admin/css/dashboardcontainer.css" />
         <link rel="stylesheet" href="../assets/global/css/table.css" />
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        
     </head>
     <body>
+
+<!-- Student View Modal -->
+<div class="modal fade" id="studentVIEWmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Customer Appointment Information</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="student_viewing_data"></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Student Delete Modal -->
+<div class="modal fade" id="deleteStudentModal" tabindex="-1" aria-labelledby="deleteStudentModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="deleteStudentModalLabel">Student Delete Data</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form action="session.php" method="POST">
+            <div class="modal-body">
+                <input type="hidden" name="user_id" id="delete_id">
+                <h4>Are You Sure you want to delete this appointment?</h4>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" name="delete_student" class="btn btn-danger" >Yes</button>
+            </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
         <div class="grid-container">
             <!-- Header -->
             <?php require "header/header.php"?>
@@ -62,24 +108,26 @@
                                     $user = $row['user_id'];    
                                     $service = $row['service_id'];
                                     $status = $row['apt_status'];
-                                    $apt_date = date("M j, Y", strtotime($row['apt_date']));
-                                }
-                            }
-                            else {echo '<tr><td colspan="5" style="text-align: center;">No records found.</td></tr>';}
+                                    $apt_date = date("M j, Y", strtotime($row['apt_date']));                                        
                             ?>
 
                                 <tr>
-                                    <td><?php echo $t++ ?></td>
-                                    <td><?php echo ($user); ?></td>
+                                    <td class="apt_no"><?php echo ($t++); ?></td>
+                                    <td class="u_id"><?php echo ($user); ?></td>
                                     <td><?php echo ($service); ?></td>
                                     <td><?php echo ($apt_date); ?></td>
                                     <td><?php echo ($status); ?></td>
                                     <td class = "crudebtn">
-                                        <button>View</button>
+                                        <button class="view_btn">View</button>
                                         <button>Accept</button>
-                                        <button>Decline</button>  
+                                        <button class="del_btn">Delete</button>  
                                     </td>
                                 </tr>
+                                <?php
+                                }}
+                                        // include('user_account_modals/user_account_view_user.php');
+                                    else {echo '<tr><td colspan="5" style="text-align: center;">No records found.</td></tr>';}
+                                ?>
                         </tbody>
                     </div>
                 </div>
@@ -89,5 +137,39 @@
         <!-- Custom JS -->
         <script src="../assets/admin/js/sidebar_toggle.js"></script>
         <script src="../assets/global/js/table.js"></script>
+        
+        <script>
+            $(document).ready(function () {
+
+                $('.del_btn').click(function (e) { 
+                    e.preventDefault();
+                    var u_id = $(this).closest('tr').find('.u_id').text();
+                    $('#delete_id').val(u_id);
+                    $('#deleteStudentModal').modal('show');
+                    
+                });
+                
+                $('.view_btn').click(function (e) { 
+                    e.preventDefault();
+                    //alert('Hellow');
+
+                    var u_id = $(this).closest('tr').find('.u_id').text();
+
+                    $.ajax({
+                        type: "POST",
+                        url: "session.php",
+                        data: {
+                                'checking_viewbtn':true,
+                                'u_id':u_id,
+                        },
+                        success: function (response) {                 
+                            $('.student_viewing_data').html(response);
+                            $('#studentVIEWmodal').modal('show');
+                        }
+                    });
+                });
+
+            });
+        </script>
     </body>
 </html>
--- admin/session.php
+++ admin/session.php
@@ -20,4 +20,46 @@
         header('Location: ../logout.php');
     }
     
+
+
+    //code for view modal
+    if(isset($_POST['checking_viewbtn']))
+    {
+       $userid = $_POST['u_id'];
+       //echo $return = $userid;
+
+       $sql = mysqli_query($connection, "SELECT * FROM appointment WHERE user_id ='$userid' ") or die(mysqli_error($connection));
+       if (mysqli_num_rows($sql) > 0){
+            foreach ($sql as $row){
+                echo $return ='
+                    <h5> User id: '.$row['user_id'].'</h5>
+                    <h5> Service id: '.$row['service_id'].'</h5>
+                    <h5> Appointment date: '.$row['apt_date'].'</h5>
+                    <h5> Appointment status: '.$row['apt_status'].'</h5>
+                ';
+       }}
+   else {
+    echo $return = "No records found";
+        }
+    }
+
+    //code for delete modal
+
+    if(isset($_POST['delete_student'])){
+
+        $id = $_POST['user_id'];
+
+        $query ="DELETE FROM appointment WHERE user_id='$id'";
+        $query_run = mysqli_query($connection,$query);
+
+        if($query_run){
+            $_SESSION['status'] = "Successfully Updated";
+            header('Location: scheduled_task.php');
+        }
+        else {
+            $_SESSION['status'] = "Something went wrong";
+            header('Location: scheduled_task.php');
+        }
+
+    }
 ?>
