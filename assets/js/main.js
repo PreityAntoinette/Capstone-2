852ede2832da17e15203abe876a38ef2684d21e6
jayson-tindugan <jayson.tindugan03@gmail.com>
2023-12-03 22:02:01 +0800

    fixed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,68 @@
 // change nav style on scroll
-window.addEventListener('scroll',()=>{
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
-})
-
+window.addEventListener("scroll", () => {
+    document
+        .querySelector("nav")
+        .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 // CONTACT BUTTONS (CIRCULAR TEXT BUTTONS)
 
-const textButtons = document.querySelectorAll('.contact__btn');
+const textButtons = document.querySelectorAll(".contact__btn");
 
-textButtons.forEach(textButton =>{
-    let text = textButton.querySelector('p');
+textButtons.forEach((textButton) => {
+    let text = textButton.querySelector("p");
 
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
-
+    text.innerHTML = text.innerHTML
+        .split("")
+        .map(
+            (character, index) =>
+                `<span style="transform: rotate(${
+                    index * 12
+                }deg)">${character}</span>`
+        )
+        .join("");
+});
 
 // SWIPER JS (gallery section)
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 20,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     breakpoints: {
         599: {
             slidesPerView: 2,
             spaceBetween: 40,
         },
-        1023:{
+        1023: {
             slidesPerView: 3,
             spaceBetween: 60,
-        }
-    }
-  });
+        },
+    },
+});
 
-  const nav = document.querySelector('.nav__links');
-  const openNavBtn = document.querySelector('#nav__toggle-open');
-  const closeNavBtn = document.querySelector('#nav__toggle-close');
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
 
-  const openNav= () => {
-    nav.style.display = 'flex';
-    openNavBtn.style.display = 'none';
-    closeNavBtn.style.display = 'inline-block';
-  }
+const openNav = () => {
+    nav.style.display = "flex";
+    openNavBtn.style.display = "none";
+    closeNavBtn.style.display = "inline-block";
+};
 
-  openNavBtn.addEventListener('click',openNav);
+openNavBtn.addEventListener("click", openNav);
 
-  const closeNav= () => {
-    nav.style.display = 'none';
-    openNavBtn.style.display = 'inline-block';
-    closeNavBtn.style.display = 'none';
-  }
+const closeNav = () => {
+    nav.style.display = "none";
+    openNavBtn.style.display = "inline-block";
+    closeNavBtn.style.display = "none";
+};
 
-    closeNavBtn.addEventListener('click',closeNav);
+closeNavBtn.addEventListener("click", closeNav);
 
-    nav.querySelectorAll('li a').forEach(navLink => {
-        navLink.addEventListener('click',closeNav);
-    })
-
+// nav.querySelectorAll("li a").forEach((navLink) => {
+//     navLink.addEventListener("click", closeNav);
+// });
--- index.php
+++ index.php
@@ -200,18 +200,6 @@
         </ul>
     </div>
 </footer>
-
-
-
-
-
-
-
-
-
-
-
-
     <!--swiper js cnd-->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="assets/js/main.js"></script>
