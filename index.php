6002df9d19fa8cb3ac2aa944134450ef276e09da
PreityE <Preityantoinette0521@gmail.com>
2023-12-11 00:29:36 +0800

    LAyout

--- index.php
+++ index.php
@@ -331,8 +331,9 @@
     </div>
 </footer>
     <!--swiper js cnd-->
+    
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="assets/js/main.js"></script>
-    <script src="assets/js/image.js"></script>
+    <script src="assets/global/js/services.js"></script>
 </body>
 </html>
