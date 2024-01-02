ffc0c38adf0cce9470d7070b45b309e4af71740e
PreityE <Preityantoinette0521@gmail.com>
2024-01-02 18:38:12 +0800

    heldlds

--- lagring_studio_db.sql
+++ lagring_studio_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 12, 2023 at 03:12 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 02, 2024 at 10:36 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,18 +27,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `appointment`
 --
 
-CREATE TABLE `appointment` (
-  `apt_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `service_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `apt_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `service_id` int NOT NULL,
   `apt_occasion_type` varchar(200) NOT NULL,
   `apt_date` date NOT NULL,
   `apt_time` time NOT NULL,
   `apt_status` varchar(200) NOT NULL DEFAULT 'PENDING',
   `apt_remark` varchar(500) NOT NULL DEFAULT 'Please wait for approval.',
   `apt_status_date` datetime DEFAULT NULL,
-  `apt_date_added` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `apt_date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`apt_id`),
+  KEY `service_id` (`service_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `appointment`
@@ -48,7 +52,11 @@ INSERT INTO `appointment` (`apt_id`, `user_id`, `service_id`, `apt_occasion_type
 (1, 15, 2023001, 'debut', '2023-12-04', '00:00:00', 'PENDING', 'Please wait for approval.', NULL, '2023-12-06 17:12:52'),
 (6, 15, 2023008, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-07 00:00:00', '2023-12-04 03:23:40'),
 (7, 15, 2023008, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-07 00:00:00', '2023-12-04 03:25:14'),
-(8, 15, 2023005, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-08 00:00:00', '2023-12-04 03:36:55');
+(8, 15, 2023005, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-08 00:00:00', '2023-12-04 03:36:55'),
+(9, 15, 2023003, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-05 00:00:00', '2023-12-04 11:57:34'),
+(10, 15, 2023003, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-05 00:00:00', '2023-12-04 11:58:46'),
+(11, 15, 2023003, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-05 00:00:00', '2023-12-04 12:00:00'),
+(12, 15, 2023003, 'DEBUT', '0000-00-00', '00:00:00', 'PENDING', 'Please wait for approval.', '2023-12-05 00:00:00', '2023-12-04 12:00:38');
 
 -- --------------------------------------------------------
 
@@ -56,31 +64,32 @@ INSERT INTO `appointment` (`apt_id`, `user_id`, `service_id`, `apt_occasion_type
 -- Table structure for table `services`
 --
 
-CREATE TABLE `services` (
-  `service_id` int(11) NOT NULL,
-  `serviceID` varchar(20) NOT NULL,
+DROP TABLE IF EXISTS `services`;
+CREATE TABLE IF NOT EXISTS `services` (
+  `service_id` int NOT NULL AUTO_INCREMENT,
   `service_name` varchar(250) NOT NULL,
   `service_description` varchar(500) NOT NULL,
-  `service_price` int(11) DEFAULT NULL,
-  `service_date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `archived_flag` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `service_price` int DEFAULT NULL,
+  `service_date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `archived_flag` int NOT NULL,
+  PRIMARY KEY (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2023012 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `services`
 --
 
-INSERT INTO `services` (`service_id`, `serviceID`, `service_name`, `service_description`, `service_price`, `service_date_added`, `archived_flag`) VALUES
-(2023001, 'PT', 'Portraits\r\n(Whole/Half Body)', '4r(2pcs)', 120, '2023-11-29 18:58:11', 1),
-(2023002, 'GP', 'Graduation Picture', '8r(1pc) 3r(1pc) 2r(4pcs)', 300, '2023-11-29 19:01:31', 1),
-(2023003, 'FP1', 'Family Package A', '8r(1pc) 3r(1pc) 2r(4pcs)', 300, '2023-11-29 19:04:00', 1),
-(2023004, 'FP2', 'Family Package B', '8r(2pcs) 3r(3pcs) 2r(4pcs)', 350, '2023-11-29 19:05:42', 1),
-(2023005, 'DP', 'Digital Package', 'Unlimited Shots,\r\n8x12(40pcs) w/ layout & album,\r\nVideo Coverage 2pcs flash-drives,\r\nFree 1pc 16x20 Blow up pic w/ frame plus 1 free signature frame ', NULL, '2023-11-29 19:07:48', 1),
-(2023006, 'SP', 'Special Package', 'Unlimited Shots,\r\n5r(100pcs) w/ layout & Album,\r\nVideo Coverage 2pcs flash-drives,\r\nFREE 11x14 blow up pic w/ frame and signature frame(1pc)', NULL, '2023-11-29 19:21:10', 1),
-(2023007, 'RP', 'Regular Package', 'Unlimited Shots,\r\n5r(80pcs) & album,\r\nVideo coverage 1 flash-drive', NULL, '2023-11-29 19:28:11', 1),
-(2023008, 'BP', 'Budget Package', 'Unlimited Shots,\r\n5r(100pcs)', NULL, '2023-11-29 19:30:35', 1),
-(2023009, 'HP', 'Hire a Photographer', 'Photo only', NULL, '2023-11-29 19:34:15', 1),
-(2023010, 'HVG', 'Hire a Video Grapher', 'Video Only', NULL, '2023-11-29 19:35:26', 1);
+INSERT INTO `services` (`service_id`, `service_name`, `service_description`, `service_price`, `service_date_added`, `archived_flag`) VALUES
+(2023001, 'Portraits\r\n(Whole/Half Body)', '4r(2pcs)', 120, '2023-11-29 18:58:11', 1),
+(2023002, 'Graduation Picture', '8r(1pc) 3r(1pc) 2r(4pcs)', 300, '2023-11-29 19:01:31', 1),
+(2023003, 'Family Package A', '8r(1pc) 3r(1pc) 2r(4pcs)', 300, '2023-11-29 19:04:00', 1),
+(2023004, 'Family Package B', '8r(2pcs) 3r(3pcs) 2r(4pcs)', 350, '2023-11-29 19:05:42', 1),
+(2023005, 'Digital Package', 'Unlimited Shots,\r\n8x12(40pcs) w/ layout & album,\r\nVideo Coverage 2pcs flash-drives,\r\nFree 1pc 16x20 Blow up pic w/ frame plus 1 free signature frame ', NULL, '2023-11-29 19:07:48', 1),
+(2023006, 'Special Package', 'Unlimited Shots,\r\n5r(100pcs) w/ layout & Album,\r\nVideo Coverage 2pcs flash-drives,\r\nFREE 11x14 blow up pic w/ frame and signature frame(1pc)', NULL, '2023-11-29 19:21:10', 1),
+(2023007, 'Regular Package', 'Unlimited Shots,\r\n5r(80pcs) & album,\r\nVideo coverage 1 flash-drive', NULL, '2023-11-29 19:28:11', 1),
+(2023008, 'Budget Package', 'Unlimited Shots,\r\n5r(100pcs)', NULL, '2023-11-29 19:30:35', 1),
+(2023009, 'Hire a Photographer', 'Photo only', NULL, '2023-11-29 19:34:15', 1),
+(2023010, 'Hire a Video Grapher', 'Video Only', NULL, '2023-11-29 19:35:26', 1);
 
 -- --------------------------------------------------------
 
@@ -88,84 +97,64 @@ INSERT INTO `services` (`service_id`, `serviceID`, `service_name`, `service_desc
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `firstname` varchar(200) NOT NULL,
   `middlename` varchar(200) NOT NULL,
   `surname` varchar(200) NOT NULL,
-  `user_contact` bigint(20) NOT NULL,
-  `registration_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `registration_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `contact` int NOT NULL,
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `role` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `role` varchar(200) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `firstname`, `middlename`, `surname`, `user_contact`, `registration_date`, `email`, `password`, `role`) VALUES
-(2, 'Admin', 'Mname', 'Surname', 0, '2023-10-22 16:45:44', 'admin.admin@cvsu.edu.ph', '$2y$10$OMyazDWKGNERSJW6KIfPIuZG1/ajR/tnen0g5q/zMoOm4cteM4EjK', 'ADMIN'),
-(15, 'fname', 'mname', 'sname', 0, '2023-11-28 23:48:10', 'user.user@cvsu.edu.ph', '$2y$10$AycJciKiIN1xK6pxtFpXpeT8iQAMIysmH6W7MZ1aWWUwEZ0QyZttK', 'USER');
+INSERT INTO `users` (`user_id`, `firstname`, `middlename`, `surname`, `registration_date`, `contact`, `email`, `password`, `role`) VALUES
+(2, 'Admin', 'Mname', 'Surname', '2023-10-22 16:45:44', 0, 'admin.admin@cvsu.edu.ph', '$2y$10$OMyazDWKGNERSJW6KIfPIuZG1/ajR/tnen0g5q/zMoOm4cteM4EjK', 'ADMIN'),
+(15, 'fname', 'mname', 'sname', '2023-11-28 23:48:10', 0, 'user.user@cvsu.edu.ph', '$2y$10$AycJciKiIN1xK6pxtFpXpeT8iQAMIysmH6W7MZ1aWWUwEZ0QyZttK', 'USER');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users_step`
+-- Table structure for table `website`
 --
 
-CREATE TABLE `users_step` (
-  `user_id` int(11) NOT NULL,
-  `step` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `website`;
+CREATE TABLE IF NOT EXISTS `website` (
+  `heading_title` varchar(200) NOT NULL,
+  `heading_paragraph` varchar(500) NOT NULL,
+  `services_paragraph` varchar(200) NOT NULL,
+  `about_timeopen` varchar(500) NOT NULL,
+  `about_paragraph` varchar(1000) NOT NULL,
+  `footer` varchar(200) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `website`
+--
+
+INSERT INTO `website` (`heading_title`, `heading_paragraph`, `services_paragraph`, `about_timeopen`, `about_paragraph`, `footer`) VALUES
+('Capture every moment with us', ' At Lagring studio, we believe in the power of creativity, technology, and imagination. We are thrilled to introduce our cutting-edge digital studio, where we transform ideas into captivating digital experiences. Allow us to join you in your every adventure and milestones in life and together, lets treasure every moment.', 'The following are our budget friendly but quality services. Contact us for more details. Book now!', 'Monday to Sunday\r\n8am to 5pm', ' The backround of Lagring Studio way back in 2010, There was a woman named Alegria Garcia who established a photo studio in Salitran and named the studio \"Lagring Studio\". Alegria “Lagring” Garcia is a mother of 4 child. The humble start of Lagring Studio shows that persistence is important if you want to be successful.\r\n\r\n    The business started with a small studio offering ID pictures, photo and video packages to events.The studio also sold frames in different size and photo enlargement. Through years of business, they establish their name in picture industry specially in Imus. They now cover almost 70% of public schools in Imus such as the big school in Imus National High School (INHS), Gen. Emilio Aguinald National High School (GEANHS), Malagasang 1,2,3 Elementary School, etc.. Until now, the business continues to grow.', 'Support Lagringstudio@gmail.com'),
+('', ' At Lagring studio, we believe in the power of creativity, technology, and imagination. We are thrilled to introduce our cutting-edge digital studio, where we transform ideas into captivating digital experiences. Allow us to join you in your every adventure and milestones in life and together, lets treasure every moment.', '', 'Call Us: +63915 229 824\r\nOur Email: Lagringstudio@gmail.com', '', '');
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `appointment`
+-- Constraints for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`apt_id`),
-  ADD KEY `service_id` (`service_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
 
---
--- Indexes for table `services`
---
-ALTER TABLE `services`
-  ADD PRIMARY KEY (`service_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `users_step`
---
-ALTER TABLE `users_step`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
-
---
-
---
--- AUTO_INCREMENT for table `appointment`
---
-ALTER TABLE `appointment`
-  MODIFY `apt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `services`
---
-ALTER TABLE `services`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2023012;
-
---
--- AUTO_INCREMENT for table `users`
---
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
